a0c519fa53e67390fec029aa419d742df50df1c1
MGausseran <martin.gausseran@gmail.com>
2024-06-19 16:45:49 +0200

    Tentative d'implémentation de prix

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 let multiplicateur = 1;
 let scoreParClic = 1;
-let score = document.getElementById("points").textContent;
+let score = parseInt(document.getElementById("points").textContent);
 
-document.getElementById("Mike").addEventListener("click", function(){
-    score++
+function ajouterMultiplicateur(valeur) {
+    multiplicateur *= valeur;
+    alert("Votre multiplicateur actuel est de :" + multiplicateur);
+}
+
+document.getElementById("Mike").addEventListener("click", function () {
+    score += scoreParClic * multiplicateur;
+    document.getElementById("points").textContent = score;
     console.log(score)
 }
 )
+
+document.getElementById("X2").addEventListener("click", function () {
+    ajouterMultiplicateur(2);
+    if (score <= 100) {
+        
+    }
+})
+
+document.getElementById("X3").addEventListener("click", function () {
+    ajouterMultiplicateur(3);
+})
+
+document.getElementById("X4").addEventListener("click", function () {
+    ajouterMultiplicateur(4);
+    score -= 1000;
+})
+
+document.getElementById("XBonus").addEventListener("click", function () {
+    ajouterMultiplicateur(200)
+    setTimeout(30000);
+})

950945214fdd26ae7cb62fc8190825879755d8db
MGausseran <martin.gausseran@gmail.com>
2024-06-19 15:12:26 +0200

    Multiplicateurs fonctionnels

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ document.getElementById("Mike").addEventListener("click", function(){
     console.log(score)
 }
 )
+
+document.getElementById("X2").addEventListener("click", function(){
+    multiplicateur+=2;
+    console.log(multiplicateur);
+    scoreParClic*=multiplicateur
+    console.log(scoreParClic)
+})
+document.getElementById("X3").addEventListener("click", function(){
+    multiplicateur+=3;
+    console.log(multiplicateur);
+    scoreParClic*=multiplicateur
+    console.log(scoreParClic)
+})
+document.getElementById("X4").addEventListener("click", function(){
+    multiplicateur+=4;
+    console.log(multiplicateur);
+    scoreParClic*=multiplicateur
+    console.log(scoreParClic)
+})
+document.getElementById("XBonus").addEventListener("click", function(){
+    multiplicateur+=200;
+    console.log(multiplicateur);
+    scoreParClic*=multiplicateur
+    console.log(scoreParClic)
+    setTimeout(30000);
+})

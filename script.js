10407b3dfead6937ef10d90ae1d3561ff122d4b8
Martin Gausseran <martin.gausseran@gmail.com>
2024-06-20 07:33:38 +0200

    Ajout d'un prix pour les multiplicateurs, qui se déduit du score à chaque achat

--- script.js
+++ script.js
@@ -2,34 +2,37 @@ let multiplicateur = 1;
 let scoreParClic = 1;
 let score = parseInt(document.getElementById("points").textContent);
 
-function ajouterMultiplicateur(valeur) {
-    multiplicateur *= valeur;
-    alert("Votre multiplicateur actuel est de :" + multiplicateur);
-}
-
-document.getElementById("Mike").addEventListener("click", function () {
-    score += scoreParClic * multiplicateur;
-    document.getElementById("points").textContent = score;
-    console.log(score)
-}
-)
-
-document.getElementById("X2").addEventListener("click", function () {
-    ajouterMultiplicateur(2);
-    if (score <= 100) {   
+function ajouterMultiplicateur(valeur, prix) {
+    if (score >= prix) {
+        score -= prix;
+        multiplicateur *= valeur;
+        alert("Votre multiplicateur actuel est de :" + multiplicateur);
     }
-})
+    else {
+        alert("Vous n'avez pas assez de Mikes pour acheter ce multiplicateur !")
+    }
+}
+    document.getElementById("Mike").addEventListener("click", function () {
+        score += scoreParClic * multiplicateur;
+        document.getElementById("points").textContent = score;
+        console.log(score)
+    }
+    )
 
-document.getElementById("X3").addEventListener("click", function () {
-    ajouterMultiplicateur(3);
-})
+    document.getElementById("X2").addEventListener("click", function () {
+        ajouterMultiplicateur(2, 5);
+    })
 
-document.getElementById("X4").addEventListener("click", function () {
-    ajouterMultiplicateur(4);
-    score -= 1000;
-})
+    document.getElementById("X3").addEventListener("click", function () {
+        ajouterMultiplicateur(3, 500);
+    })
 
-document.getElementById("XBonus").addEventListener("click", function () {
-    ajouterMultiplicateur(200)
-    setTimeout(30000);
-})
+    document.getElementById("X4").addEventListener("click", function () {
+        ajouterMultiplicateur(4, 1000);
+        score -= 1000;
+    })
+
+    document.getElementById("XBonus").addEventListener("click", function () {
+        ajouterMultiplicateur(200, 5000)
+        setTimeout(30000);
+    })

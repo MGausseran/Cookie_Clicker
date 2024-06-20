a827950fe818e2f5fa4b26a2f58695818067e9f4
Martin Gausseran <martin.gausseran@gmail.com>
2024-06-20 07:55:42 +0200

    Ajout de l'augmentation systématique des prix après achat

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 let multiplicateur = 1;
 let scoreParClic = 1;
 let score = parseInt(document.getElementById("points").textContent);
+let prixMultiX2 = 5;
+let prixMultiX3 = 500;
+let prixMultiX4 = 1000;
+let prixBonus = 5000;
 
 function ajouterMultiplicateur(valeur, prix) {
     if (score >= prix) {
         score -= prix;
         multiplicateur *= valeur;
-        alert("Votre multiplicateur actuel est de :" + multiplicateur);
+        alert("Votre multiplicateur actuel est de :" + multiplicateur);      
     }
     else {
         alert("Vous n'avez pas assez de Mikes pour acheter ce multiplicateur !")
     }
 }
+
+
+
     document.getElementById("Mike").addEventListener("click", function () {
         score += scoreParClic * multiplicateur;
         document.getElementById("points").textContent = score;
@@ -20,19 +27,21 @@ function ajouterMultiplicateur(valeur, prix) {
     )
 
     document.getElementById("X2").addEventListener("click", function () {
-        ajouterMultiplicateur(2, 5);
+        ajouterMultiplicateur(2, prixMultiX2);
+        prixMultiX2 *= 2;
+        alert("Le prix du multiplicateur x2 est désormais de " + prixMultiX2)
     })
 
     document.getElementById("X3").addEventListener("click", function () {
-        ajouterMultiplicateur(3, 500);
+        ajouterMultiplicateur(3, prixMultiX3);
     })
 
     document.getElementById("X4").addEventListener("click", function () {
-        ajouterMultiplicateur(4, 1000);
+        ajouterMultiplicateur(4, prixMultiX4);
         score -= 1000;
     })
 
     document.getElementById("XBonus").addEventListener("click", function () {
-        ajouterMultiplicateur(200, 5000)
+        ajouterMultiplicateur(200, prixBonus)
         setTimeout(30000);
     })

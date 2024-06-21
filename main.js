d7099a8b738c38828ac4bed3f624677751e3b997
LucasMaroy <lucasmaroy42@gmail.com>
2024-06-21 13:53:13 +0200

    Front End - Welcome Page + Game Page

---
+++ main.js
@@ -0,0 +1,3 @@
+import './input.css'
+import viteLogo from '/vite.svg'
+// import { setupCounter } from './script.js'
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "#16232E",
+        lightblue: "#63C3D1" ,
+        midblue: "#245060",
+      }
+    },
+  },
+  plugins: [],
+}
+

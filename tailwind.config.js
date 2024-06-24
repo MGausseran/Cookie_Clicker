c9dc2a81496fd7b27c40867f3f093a354393a895
LucasMaroy <lucasmaroy42@gmail.com>
2024-06-24 13:58:29 +0200

    Front End - update id inventory/update V0.5 responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      'bxl': '2180px',
+      '2bxl': '2560px',
+      '3bxl': '3072px',
+    },
     extend: {
       colors: {
         darkblue: "#16232E",
